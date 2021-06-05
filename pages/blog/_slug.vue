80b9710231657c9527b7c93a71fe0f568b394dee
trevorsmithdev <trevor.smith.dev@gmail.com>
2021-06-05 18:15:15 -0500

    disable eslint object-shorthand

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,5 +1,6 @@
 <template>
   <article>
+    <p>Post last updated: {{ formatDate(post.updatedAt) }}</p>
     <nuxt-content :document="post" />
     <pre> {{ post }} </pre>
   </article>
@@ -7,9 +8,26 @@
 
 <script>
 export default {
-  async asyncData({ $content, params }) {
+  asyncData: async function ({ $content, params }) {
     const post = await $content('posts', params.slug).fetch()
     return { post }
   },
+  methods: {
+    // eslint-disable-next-line object-shorthand
+    formatDate: function (date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    },
+  },
 }
 </script>
+
+<style>
+h1,
+h2,
+h3,
+h4,
+p {
+  margin-bottom: 1em;
+}
+</style>
